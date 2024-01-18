41a5fe720fb73931f7ffe2e2b710ee2dc16ece03
olyshaaa <olgasevel108@gmail.com>
2024-01-18 17:00:10 +0200

    products page

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,6 +1,54 @@
+import Image from "next/image"
+import Link from "next/link"
+import styles from "../../ui/dashboard/products/products.module.css"
+import Search from "../../ui/dashboard/search/search"
+import Pagination from "../../ui/dashboard/pagination/pagination"
+
 const Products = () => {
     return (
-        <div>Products</div>
+            <div className={styles.container}>
+            <div className={styles.top}>
+                <Search placeholder="Search for a product..."/>
+                <Link href="/dasboard/products/add">
+                    <button className={styles.addButton}>Add New</button>
+                </Link>
+            </div>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <td>Title</td>
+                        <td>Description</td>
+                        <td>Price</td>
+                        <td>Created at</td>
+                        <td>Stock</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className={styles.product}>
+                                <Image src="/noproduct.jpg" alt="" width={40} height={40} className={styles.productImage}/>
+                                IPhone
+                            </div>
+                        </td>
+                        <td>Desc</td>
+                        <td>15.05.2023</td>
+                        <td>$1500</td>
+                        <td>42</td>
+                        <td>
+                            <div className={styles.buttons}>
+                                <Link href="/">
+                                    <button className={`${styles.button} ${styles.view}`}>View</button>
+                                </Link>
+                                <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <Pagination />
+        </div>
     )
 }
 
--- app/ui/dashboard/footer/footer.jsx
+++ app/ui/dashboard/footer/footer.jsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css"
 const Footer = () =>{
     return (
         <div className={styles.container}>
-            <div className={styles.logo}>Olga Shevel</div>
+            <div className={styles.logo}>Olha Shevel</div>
             <div className={styles.text}>All rights reserved.</div>
         </div>
     )
