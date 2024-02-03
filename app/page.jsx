eaef0bcf3d2404bb18d5e138fc45b166c2e6bc4b
olyshaaa <olgasevel108@gmail.com>
2024-02-03 15:49:34 +0200

    router homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,13 @@
+"use client"
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
 const Homepage = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push("/login")
+  }, [])
   return (
     <div>Homepage</div>
   )
